cfa87a523de29db7305fb906e1f7c54ec02fbfe3
laksac24 <lakshyasachdeva24@gmail.com>
2025-04-20 18:38:30 +0530

    initial commit

--- app.py
+++ app.py
@@ -103,5 +103,8 @@ def predict():
         "predicted_team": predicted_team
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
